a35b93b1f0597b6e7abbd59dde0713c84139193a
denibudeyko <denbudeyko@gmail.com>
2020-09-01 11:38:51 +0300

    Add new task

--- js/app.js
+++ js/app.js
@@ -1,6 +1,52 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+
+	const input = document.getElementsByClassName('new-todo')[0]
+	const todoList =  document.getElementsByClassName('todo-list')[0]
+
+	
+	input.addEventListener('keyup', function (e) {
+		
+		var string = input.value;
+
+		if(e.key == 'Enter'){
+			
+
+
+			var liClass = document.createElement('li');
+			var divClass = document.createElement('div');
+			var editInput = document.createElement('input')
+			var toggleInput = document.createElement('input')
+			var labelInput = document.createElement('label')
+			var button =  document.createElement('button')
+
+			divClass.className = 'view';
+			editInput.className = 'edit';
+			editInput.value = 'Rule the web';
+			toggleInput.className = 'toggle';
+			toggleInput.type = 'checkbox';
+			labelInput.textContent = string;
+			button.className = 'destroy';
+
+
+			liClass.appendChild(divClass);
+			liClass.appendChild(editInput)
+			divClass.appendChild(toggleInput)
+			divClass.appendChild(labelInput)
+			divClass.appendChild(button)
+			todoList.appendChild(liClass);
+
+
+			input.value = '';
+
+
+		}
+		
+	})
+	
+
+
+
 
 })(window);
