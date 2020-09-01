7afbdad37ebbbacd10936f935558a1eec9db38ff
denibudeyko <denbudeyko@gmail.com>
2020-09-01 18:03:14 +0300

    update

--- js/app.js
+++ js/app.js
@@ -2,13 +2,23 @@
 	'use strict';
 
 
+	// Initial Const 
 	const input = document.getElementsByClassName('new-todo')[0]
 	const todoList =  document.getElementsByClassName('todo-list')[0]
 	const items = document.querySelectorAll('.todo-list li')
+	const notCompletedItems = document.querySelectorAll('.todo-list li:not(.completed)')
 	const clear = document.querySelector('.clear-completed')
+	const todoCount = document.querySelector('.todo-count strong')
+	
+	
+	function updateCount(){
+		todoCount.textContent = document.querySelectorAll('.todo-list li:not(.completed)').length;
+	}
 
+	// Initial Item left	
+	updateCount()
 	
-	
+
 	input.addEventListener('keyup', function (e) {
 		
 		var string = input.value;
@@ -28,19 +38,17 @@
 
 			todoList.appendChild(li);
 			const childrenItem = li.querySelector('input.toggle')
+			
 
 			childrenItem.addEventListener('click', event => {
 
-				
 				toggleTaskStatusEvent(event.target)
-
-
 
 			})
 
 
 			input.value = '';
-
+			updateCount();
 
 		}
 		
@@ -63,6 +71,7 @@
 	function toggleTaskStatusEvent(e){
 		const liParent = e.closest('li')
 		liParent.classList.toggle('completed')
+		updateCount()
 	}
 
 	clear.addEventListener('click', function(){
