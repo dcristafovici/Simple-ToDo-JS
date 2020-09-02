c35ece6eb76c9f75f4f0e6ed1ea22fce878ddc50
denibudeyko <denbudeyko@gmail.com>
2020-09-02 10:27:09 +0300

    add app projects

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@
 	const todoList =  document.getElementsByClassName('todo-list')[0]
 	const items = document.querySelectorAll('.todo-list li')
 	const notCompletedItems = document.querySelectorAll('.todo-list li:not(.completed)')
+	const completedItems = document.querySelectorAll('.todo-list li.completed')
 	const clear = document.querySelector('.clear-completed')
 	const todoCount = document.querySelector('.todo-count strong')
 	
@@ -78,15 +79,73 @@
 
 		document.querySelectorAll('.todo-list li.completed').forEach(item=>{
 
-			item.style.display = 'none';
+			item.remove()
 
 		})
+
+
 
 
 		
 	})
 
+	
+	// FiltersButtons
+	document.querySelectorAll('ul.filters li').forEach(item=>{
 
+		const filterButton = item.children[0];
+		filterButton.addEventListener('click', event=>{
+
+			runFilter(event.target)
+
+
+		})
+		
+	})
+
+
+	function runFilter(item){
+
+		const notCompletedItemsFilter  = document.querySelectorAll('.todo-list li:not(.completed)')
+		const completedItemsFilter = document.querySelectorAll('.todo-list li.completed')
+		const allItemsFilter = document.querySelectorAll('.todo-list li');
+
+		var href = item.getAttribute('href')
+		href = href.split('#/')[1]
+
+		const activeButton = document.querySelector('ul.filters a.selected')
+		activeButton.classList.remove('selected')
+		item.classList.add("selected")
+
+
+		
+		if(href == 'active'){
+
+			notCompletedItemsFilter.forEach(item=>{
+				item.style.display = 'block';
+			})
+			completedItemsFilter.forEach(item=>{
+				item.style.display = 'none';
+			})
+
+		}
+		else if(href == 'completed'){
+			notCompletedItemsFilter.forEach(item=>{
+				item.style.display = 'none';
+			})
+			completedItemsFilter.forEach(item=>{
+				item.style.display = 'block';
+			})
+		}
+
+		else if(href == 'all'){
+			allItemsFilter.forEach(item=>{
+				item.style.display = 'block'
+			})
+		}
+	}
+
+	
 
 
 
